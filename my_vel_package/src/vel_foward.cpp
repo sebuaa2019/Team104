30bb66d9126a6a3e3da92e1eee4321f9e8e53856
Michael <michael.201096@gmail.com>
2019-05-28 01:36:59 +0800

    update my_vel_package

---
+++ my_vel_package/src/vel_foward.cpp
@@ -0,0 +1,307 @@
+/*#include <ros/ros.h>
+#include <ctime>
+#include <time.h>
+#include <stdlib.h>
+#include <geometry_msgs/Twist.h>
+
+using namespace std;
+clock_t post_time, pre_time;
+
+void move_x(int argc, char** argv, int flag);
+void move_y(int argc, char** argv, int flag);
+void move_z(int argc, char** argv, int flag);
+
+int main(int argc, char** argv)
+{
+
+	    move_x(argc,argv,1);
+        move_x(argc,argv,0);    //fan fangxiang
+	    move_y(argc,argv,1);
+        move_y(argc,argv,0);    //fan fangxiang
+	    move_z(argc,argv,1);
+        move_z(argc,argv,0);    //fan fangxiang
+	
+    return 0;
+}
+void move_x(int argc, char** argv,int flag)
+{
+    ros::init(argc, argv, "vel_ctrl");
+    ros::NodeHandle n;
+    ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+    
+    pre_time = clock();cd ..
+
+    while(ros::ok())
+    {
+	    post_time = clock();
+		if (post_time - pre_time >= 1000)
+			break;
+        geometry_msgs::Twist vel_cmd;
+
+        if(flag)
+            vel_cmd.linear.x = 0.1;
+        else
+            vel_cmd.linear.x = -0.1;
+
+        vel_cmd.linear.y = 0;
+        vel_cmd.linear.z = 0;
+        
+        vel_cmd.angular.x = 0;
+        vel_cmd.angular.y = 0;
+        vel_cmd.angular.z = 0;
+
+        vel_pub.publish(vel_cmd);
+	
+        ros::spinOnce();
+    }
+}
+
+void move_y(int argc, char** argv,int flag)
+{
+    ros::init(argc, argv, "vel_ctrl");
+    ros::NodeHandle n;
+    ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+    pre_time = clock();
+
+    while(ros::ok())
+    {
+		post_time = clock();
+		if (post_time - pre_time >= 1000)
+			break;
+        geometry_msgs::Twist vel_cmd;
+        vel_cmd.linear.x = 0;
+
+        if(flag)
+            vel_cmd.linear.y = 0.1;
+        else
+            vel_cmd.linear.y = -0.1;
+
+        vel_cmd.linear.z = 0;
+        
+        vel_cmd.angular.x = 0;
+        vel_cmd.angular.y = 0;
+        vel_cmd.angular.z = 0;
+
+        vel_pub.publish(vel_cmd);
+	
+        ros::spinOnce();
+    }
+}
+void move_z(int argc, char** argv,int flag)
+{
+    ros::init(argc, argv, "vel_ctrl");
+    ros::NodeHandle n;
+    ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+    pre_time = clock();
+
+    while(ros::ok())
+    {
+		post_time = clock();
+		if (post_time - pre_time >= 1000)
+			break;
+        geometry_msgs::Twist vel_cmd;
+        vel_cmd.linear.x = 0;
+        vel_cmd.linear.y = 0;
+
+        if(flag)
+            vel_cmd.linear.z = 0.1;
+        else
+            vel_cmd.linear.z = 0.1;
+        
+        vel_cmd.angular.x = 0;
+        vel_cmd.angular.y = 0;
+        vel_cmd.angular.z = 0;
+
+        vel_pub.publish(vel_cmd);
+	
+        ros::spinOnce();
+    }
+}
+*/
+
+
+#include <ros/ros.h>
+#include <stdlib.h>
+#include <geometry_msgs/Twist.h>
+
+#define time_start 1000000
+#define time_stop 1000000
+
+using namespace std;
+int times;
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "vel_ctrl");
+    ros::NodeHandle n;
+    ros::Publisher vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+    times = time_start; 
+    while(ros::ok() && times)
+    {
+        geometry_msgs::Twist vel_cmd;
+        vel_cmd.linear.x = 0.1;
+        vel_cmd.linear.y = 0;
+        vel_cmd.linear.z = 0;
+        
+        vel_cmd.angular.x = 0;
+        vel_cmd.angular.y = 0;
+        vel_cmd.angular.z = 0;
+
+        vel_pub.publish(vel_cmd);
+         times--;
+        ros::spinOnce();
+    }
+
+    /*times = time_start; 
+    while(ros::ok() && times)
+    {
+        geometry_msgs::Twist vel_cmd;
+        vel_cmd.linear.x = 0;
+        vel_cmd.linear.y = 0;
+        vel_cmd.linear.z = 0;
+        
+        vel_cmd.angular.x = 0;
+        vel_cmd.angular.y = 0;
+        vel_cmd.angular.z = 0;
+
+        vel_pub.publish(vel_cmd);
+         times--;
+        ros::spinOnce();
+    }*/
+return 0;
+}
+
+/*
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <signal.h>
+#include <termios.h>
+#include <stdio.h>
+
+#define KEYCODE_R 0x43 
+#define KEYCODE_L 0x44
+#define KEYCODE_U 0x41
+#define KEYCODE_D 0x42
+#define KEYCODE_Q 0x71
+
+class TeleopTurtle
+{
+public:
+  TeleopTurtle();
+  void keyLoop();
+
+private:
+
+  
+  ros::NodeHandle nh_;
+  double linear_, angular_, l_scale_, a_scale_;
+  ros::Publisher twist_pub_;
+  
+};
+
+TeleopTurtle::TeleopTurtle():
+  linear_(0),
+  angular_(0),
+  l_scale_(2.0),
+  a_scale_(2.0)
+{
+  nh_.param("scale_angular", a_scale_, a_scale_);
+  nh_.param("scale_linear", l_scale_, l_scale_);
+
+  twist_pub_ = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+}
+
+int kfd = 0;
+struct termios cooked, raw;
+
+void quit(int sig)
+{
+  (void)sig;
+  tcsetattr(kfd, TCSANOW, &cooked);
+  ros::shutdown();
+  exit(0);
+}
+
+
+int main(int argc, char** argv)
+{ 
+  ros::init(argc, argv, "vel_ctrl");
+  TeleopTurtle teleop_turtle;
+
+  signal(SIGINT,quit);
+
+  teleop_turtle.keyLoop();
+  
+  return(0);
+}
+
+
+void TeleopTurtle::keyLoop()
+{
+  char c;
+  bool dirty=false;
+
+
+  // get the console in raw mode                                                              
+  tcgetattr(kfd, &cooked);
+  memcpy(&raw, &cooked, sizeof(struct termios));
+  raw.c_lflag &=~ (ICANON | ECHO);
+  // Setting a new line, then end of file                         
+  raw.c_cc[VEOL] = 1;
+  raw.c_cc[VEOF] = 2;
+  tcsetattr(kfd, TCSANOW, &raw);
+
+  puts("Reading from keyboard");
+  puts("---------------------------");
+  puts("Use arrow keys to move the turtle.");
+
+
+  for(;;)
+  {
+    // get the next event from the keyboard  
+    if(read(kfd, &c, 1) < 0)
+    {
+      perror("read():");
+      exit(-1);
+    }
+
+    linear_=angular_=0;
+    ROS_DEBUG("value: 0x%02X\n", c);
+  
+    switch(c)
+    {
+      case KEYCODE_L:
+        ROS_DEBUG("LEFT");
+        angular_ = 0.1;
+        dirty = true;
+        break;
+      case KEYCODE_R:
+        ROS_DEBUG("RIGHT");
+        angular_ = -0.1;
+        dirty = true;
+        break;
+      case KEYCODE_U:
+        ROS_DEBUG("UP");
+        linear_ = 0.1;
+        dirty = true;
+        break;
+      case KEYCODE_D:
+        ROS_DEBUG("DOWN");
+        linear_ = -0.1;
+        dirty = true;
+        break;
+    }
+   
+
+    geometry_msgs::Twist twist;
+    twist.angular.z = a_scale_*angular_;
+    twist.linear.x = l_scale_*linear_;
+    if(dirty ==true)
+    {
+      twist_pub_.publish(twist);    
+      dirty=false;
+    }
+  }
+
+
+  return;
+}*/

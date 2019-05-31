8cf82b8ff07d1958c032809a1ec4c9f97a32edf8
Michael <michael.201096@gmail.com>
2019-05-31 10:05:27 +0800

    added stop, and change for the time_start and stop

--- my_vel_package/src/vel_foward.cpp
+++ my_vel_package/src/vel_foward.cpp
@@ -3,7 +3,7 @@
 #include <geometry_msgs/Twist.h>
 
 #define time_start 1500000
-#define time_stop 1000000
+#define time_stop 150000
 
 using namespace std;
 int times;
@@ -16,13 +16,13 @@ int main(int argc, char** argv)
     while(ros::ok() && times)
     {
         geometry_msgs::Twist vel_cmd;
-        vel_cmd.linear.x = 0;
+        vel_cmd.linear.x = 0.1;
         vel_cmd.linear.y = 0;
         vel_cmd.linear.z = 0;
         
         vel_cmd.angular.x = 0;
         vel_cmd.angular.y = 0;
-        vel_cmd.angular.z = 0.1;
+        vel_cmd.angular.z = 0;
 
         vel_pub.publish(vel_cmd);
          times--;

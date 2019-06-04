06e1021453c3b7694795a114bb38026fc9b4f644
yiliu1029 <48153772+yiliu1029@users.noreply.github.com>
2019-06-04 11:02:19 +0800

    添加部分测试代码

---
+++ test-code/addpoint-test.cpp
@@ -0,0 +1,85 @@
+#include <algorithm>
+#include <fstream>
+
+using namespace std;
+
+#define STATE_READY     0
+#define STATE_FOLLOW    1
+#define STATE_ASK       2
+#define STATE_GOTO      3
+#define STATE_GRAB      4
+#define STATE_COMEBACK  5
+#define STATE_PASS      6
+#define STATE_WAIT      7
+
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+static string strGoto;
+static sound_play::SoundRequest spk_msg;
+static ros::Publisher spk_pub;
+static ros::Publisher vel_pub;
+static string strToSpeak = "";
+static string strKeyWord = "";
+static ros::ServiceClient clientIAT;
+static xfyun_waterplus::IATSwitch srvIAT;
+static ros::ServiceClient cliGetWPName;
+static waterplus_map_tools::GetWaypointByName srvName;
+static ros::Publisher add_waypoint_pub;
+static ros::ServiceClient follow_start;
+static ros::ServiceClient follow_stop;
+static ros::ServiceClient follow_resume;
+static wpb_home_tutorials::Follow srvFlw;
+static ros::Publisher behaviors_pub;
+static std_msgs::String behavior_msg;
+
+static ros::Subscriber grab_result_sub;
+static ros::Subscriber pass_result_sub;
+static bool bGrabDone;
+static bool bPassDone;
+
+static int nState = STATE_READY;
+static int nDelay = 0;
+
+static vector<string> arKeyword;
+
+#include<iostream>
+using namespace std;
+
+void addpoint(char str1)
+{
+	tf::TransformListener listener;
+    tf::StampedTransform transform;
+    try
+    {
+        listener.waitForTransform("/map","/base_footprint",  ros::Time(0), ros::Duration(10.0) );
+        listener.lookupTransform("/map","/base_footprint", ros::Time(0), transform);
+    }
+    catch (tf::TransformException &ex) 
+    {
+        ROS_ERROR("[lookupTransform] %s",ex.what());
+        return;
+    }
+
+    float tx = transform.getOrigin().x();
+    float ty = transform.getOrigin().y();
+    tf::Stamped<tf::Pose> p = tf::Stamped<tf::Pose>(tf::Pose(transform.getRotation() , tf::Point(tx, ty, 0.0)), ros::Time::now(), "map");
+    geometry_msgs::PoseStamped new_pos;
+    tf::poseStampedTFToMsg(p, new_pos);
+
+    waterplus_map_tools::Waypoint new_waypoint;
+    new_waypoint.name = inStr;
+    new_waypoint.pose = new_pos.pose;
+    add_waypoint_pub.publish(new_waypoint);
+
+    ROS_WARN("[New Waypoint] %s ( %.2f , %.2f )" , new_waypoint.name.c_str(), tx, ty);
+
+    cout<<"Ìí¼Ó³É¹¦"<<end1; 
+}
+
+int main( int argc, char** argv)
+{
+	char str1;
+	cin>>str1;
+	
+	addpoit(str1);
+
+}
---
+++ test-code/speak-test.cpp
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <fstream>
+
+using namespace std;
+
+#define STATE_READY     0
+#define STATE_FOLLOW    1
+#define STATE_ASK       2
+#define STATE_GOTO      3
+#define STATE_GRAB      4
+#define STATE_COMEBACK  5
+#define STATE_PASS      6
+#define STATE_WAIT      7
+
+typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+static string strGoto;
+static sound_play::SoundRequest spk_msg;
+static ros::Publisher spk_pub;
+static ros::Publisher vel_pub;
+static string strToSpeak = "";
+static string strKeyWord = "";
+static ros::ServiceClient clientIAT;
+static xfyun_waterplus::IATSwitch srvIAT;
+static ros::ServiceClient cliGetWPName;
+static waterplus_map_tools::GetWaypointByName srvName;
+static ros::Publisher add_waypoint_pub;
+static ros::ServiceClient follow_start;
+static ros::ServiceClient follow_stop;
+static ros::ServiceClient follow_resume;
+static wpb_home_tutorials::Follow srvFlw;
+static ros::Publisher behaviors_pub;
+static std_msgs::String behavior_msg;
+
+static ros::Subscriber grab_result_sub;
+static ros::Subscriber pass_result_sub;
+static bool bGrabDone;
+static bool bPassDone;
+
+static int nState = STATE_READY;
+static int nDelay = 0;
+
+static vector<string> arKeyword;
+
+#include<iostream>
+using namespace std;
+
+void speak(char str1)
+{
+	spk_msg.arg = inStr;
+    spk_pub.publish(str1);
+    cout<<"²éÕÒ³É¹¦"<<end1; 
+}
+
+int main( int argc, char** argv)
+{
+	char str1;
+	cin>>str1;
+	
+	speak(str1);
+
+}
